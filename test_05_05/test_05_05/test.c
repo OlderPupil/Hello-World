e32f5be5a163d8dca9de343c3dfcda858d285fe6
OlderPupil <shui.cheng@qq.com>
2022-05-06 22:49:57 +0800

    作业补充

--- test_05_05/test_05_05/test.c
+++ test_05_05/test_05_05/test.c
@@ -1,28 +1,42 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <string.h>
+//
 int main()
 {
-	int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-	int x = 0;
-	int n = 0;
-	int i = 0;
-	n = sizeof(arr) / sizeof(arr[0]);
-	printf("请输入一个数：");
-	scanf("%d",&x);
-	while(1)
+	int arr[] = { 1, 2 ,3 ,4, 5 ,6, 7, 8, 9, 0 };
+	int sz = 0;
+	int left = 0;
+	int right = 0;
+	int mid = 0;
+	int k = 0;
+	printf("请输入要查找的目标数->");
+	//k = getchar();
+	scanf("%d", &k);
+	//sz = strlen(arr) / arr[0];
+	sz = sizeof(arr) / sizeof(arr[0]);
+	right = sz - 1;
+	while (left <= right)
 	{
-		if (arr[i] == x)
+		mid = (left + right) / 2;
+		if (k == arr[mid])
 		{
-			printf("该数的位置是：%d ", (i + 1));
+			printf("目标数的下标是：%d ", mid);
 			break;
 		}
-		else if(i < n)
-			i++;
-		else
+		else if (k < arr[mid])
 		{
-			printf("此值不在数列中!!!!！");
-			break;
+			right = mid-1;
+		}
+		else  if(k>arr[mid])
+		{
+			left = mid+1;
 		}
 	}
+	 if (left>right)
+	{
+		printf("找不到目标数！！！！");
+	}
+		
 	return 0;
 }
